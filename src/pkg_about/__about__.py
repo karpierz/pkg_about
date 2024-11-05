c7f8288f079f5df7efbea23452da92a9154a830c
Adam Karpierz <akarpierz@gmail.com>
2024-11-05 10:45:44 +0100

    0.0.1 (2020-10-16) ------------------ - Initial commit.

---
+++ src/pkg_about/__about__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2020-2021 Adam Karpierz
+# Licensed under the zlib/libpng License
+# https://opensource.org/licenses/Zlib
+
+__import__("_about", globals=globals(), level=1).about()
+__copyright__ = "Copyright (c) 2020-2021 {0}".format(__author__)
