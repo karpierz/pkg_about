1be89008ae63bfbde98f648974b31d8322b52115
Adam Karpierz <akarpierz@gmail.com>
2024-11-05 11:00:01 +0100

    1.0.2 (2021-07-20) ------------------ - First functional release.

--- src/pkg_about/__about__.py
+++ src/pkg_about/__about__.py
@@ -3,4 +3,4 @@
 # https://opensource.org/licenses/Zlib
 
 __import__("_about", globals=globals(), level=1).about()
-__copyright__ = "Copyright (c) 2020-2021 {0}".format(__author__)
+__copyright__ = f"Copyright (c) 2020-2021 {__author__}"
