29fac362a728e4d85cef12497bb965be5e17df38
Adam Karpierz <akarpierz@gmail.com>
2024-11-05 11:06:58 +0100

    1.0.3 (2021-10-14) ------------------ - Setup update.

--- src/pkg_about/__about__.py
+++ src/pkg_about/__about__.py
@@ -3,4 +3,4 @@
 # https://opensource.org/licenses/Zlib
 
 __import__("_about", globals=globals(), level=1).about()
-__copyright__ = f"Copyright (c) 2020-2021 {__author__}"
+__copyright__ = f"Copyright (c) 2020-2021 {__author__}"  # noqa
